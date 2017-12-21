919ce77190d908d9042db7a267d28e63186c8c95
emppuh <34755337+emppuh@users.noreply.github.com>
2017-12-22 00:55:18 +0200

    Add files via upload
    viikko 7, tehtävät 1, 2, 3, 4

---
+++ HelloworldController.java
@@ -0,0 +1,39 @@
+package application;
+
+import javafx.scene.control.TextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
+public class HelloworldController {
+	@FXML
+	private Button button;
+	@FXML
+	private Label label;
+	@FXML
+	private Button button2;
+	@FXML
+	private Label label2;
+	
+	@FXML
+	private TextField text;
+
+	@FXML
+	public void handleButtonAction(ActionEvent event) {
+		System.out.println("Hello world!");
+		label.setText("Hello world!");
+	}
+	
+	@FXML
+	public void writeText(ActionEvent event) {
+		System.out.println("toinen nappula");
+		label2.setText(text.getText());
+	}
+	
+}
